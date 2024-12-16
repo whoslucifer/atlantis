0f4ffee13e7d9eaa44cea388d7e2e1410d1691e2
鬼舞辻無惨 <samhansen.dev@gmail.com>
2024-12-16 22:32:25 +0300

    feat: EDITOR nvim

--- home.nix
+++ home.nix
@@ -5,7 +5,6 @@
 }: {
   imports = [
     ./hm-modules/default.nix
-
   ];
 
   home.username = "asherah";
@@ -27,6 +26,8 @@
     NIXPKGS_ALLOW_UNFREE = "1";
     NIXPKGS_ALLOW_INSECURE = "1";
     NIXOS_OZONE_WL = "1"; # for electron apps to use wayland
+    TERMINAL = "wezterm";
+    VISUAL = "nvim";
   };
 
   /*
