50146cef8cc8923d94ea4c841c7204e6ab0b0566
鬼舞辻無惨 <samhansen.dev@gmail.com>
2024-12-16 22:42:24 +0300

    fix(ags): added uv script imports

--- .config/ags/scripts/wayland-idle-inhibitor.py
+++ .config/ags/scripts/wayland-idle-inhibitor.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "pywayland",
+#     "setproctitle",
+# ]
+# ///
 
 import sys
 from dataclasses import dataclass
