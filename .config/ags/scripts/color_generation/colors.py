ebe9ae030c13599479e6c9c9cea41e9442124578
鬼舞辻無惨 <samhansen.dev@gmail.com>
2024-12-17 16:13:00 +0300

    fix(ags): uv imports colors.py

--- .config/ags/scripts/color_generation/colors.py
+++ .config/ags/scripts/color_generation/colors.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "material-color-utilities-python",
+# ]
+# ///
 from material_color_utilities_python import *
 from pathlib import Path
 import sys
