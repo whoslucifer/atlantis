00abdbe3417cc8f4480634265cbebcb51ae467f2
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-10 20:18:42 +0300

    fix(neovim): leader \ fixed telescope fzf

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader = "\\"
 
 vim.keymap.set("n", "<leader>aa", "<cmd>Dashboard<CR>", { desc = "Go to Dashboard Menu" })
 vim.keymap.set("n", "<leader>fa", function()
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,32 @@
 return {
-  "telescope.nvim",
+  "nvim-telescope/telescope.nvim",
   dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      hidden = true,
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--column",
-        "--hidden",
-        "--line-number",
-        "--no-heading",
-        "--smart-case",
-        "--unrestricted",
-        "--with-filename",
-      },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
     },
   },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        hidden = true,
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--column",
+          "--hidden",
+          "--line-number",
+          "--no-heading",
+          "--smart-case",
+          "--unrestricted",
+          "--with-filename",
+        },
+      },
+    })
+    telescope.load_extension("fzf")
+  end,
 }
