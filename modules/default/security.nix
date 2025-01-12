bd6cb245bf78b3ac1263272810821f7d73901061
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-12 21:53:23 +0300

    chore: fprintd

--- modules/default/security.nix
+++ modules/default/security.nix
@@ -63,4 +63,8 @@
   services.fprintd.enable = true;
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+  systemd.services.fprintd = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig.Type = "simple";
+  };
 }
