2052e87871750074df8976f79c2b359c71d1ab97
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-12 17:05:56 +0300

    feat: support ntfs drives

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,55 +1,31 @@
-{ lib, config, pkgs, ... }:
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest; # Kernel
 
-    boot = {
-      kernelPackages = pkgs.linuxPackages_latest; # Kernel
+    # This is for OBS Virtual Cam Support
+    kernelModules = ["v4l2loopback" "i2c-dev"];
+    extraModulePackages = [config.boot.kernelPackages.v4l2loopback];
 
-      /*kernelParams = [
-        "systemd.mask=systemd-vconsole-setup.service"
-        "systemd.mask=dev-tpmrm0.device" #this is to mask that stupid 1.5 mins systemd bug
-        "nowatchdog" 
-        "modprobe.blacklist=sp5100_tco" #watchdog for AMD
-        "modprobe.blacklist=iTCO_wdt" #watchdog for Intel
- 	    ];*/
-    
-      # This is for OBS Virtual Cam Support
-      kernelModules = [  "v4l2loopback" ];
-      extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    
-      initrd = { 
-        availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-        kernelModules = [ "i915" ];
-      };
+    supportedFilesystems = ["ntfs"];
 
-      # Needed For Some Steam Games
-      #kernel.sysctl = {
-      #  "vm.max_map_count" = 2147483642;
-      #};
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"];
+      kernelModules = ["i915"];
+    };
 
-  
-      # Make /tmp a tmpfs
-      tmp = {
-        useTmpfs = false;
-        tmpfsSize = "30%";
-        };
-    
-      # Appimage Support
-      binfmt.registrations.appimage = {
-        wrapInterpreterInShell = false;
-        interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-        recognitionType = "magic";
-        offset = 0;
-        mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-        magicOrExtension = ''\x7fELF....AI\x02'';
-      };
-    
-      plymouth.enable = true;
+    # Make /tmp a tmpfs
+    tmp = {
+      useTmpfs = false;
+      tmpfsSize = "30%";
+    };
   };
 
   drivers.intel.enable = true;
   vm.guest-services.enable = false;
   local.hardware-clock.enable = false;
-  
-
 }
-
