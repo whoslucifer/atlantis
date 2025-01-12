38bf0febd08018209728cc60916d4905e871c766
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-12 15:33:34 +0300

    refactor(neovim): ditched home manager

---
+++ modules/default/neovim.nix
@@ -0,0 +1,29 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    neovim-unwrapped
+    alejandra
+    black
+    gnumake
+    golangci-lint
+    gopls
+    gotools
+    hadolint
+    isort
+    lua-language-server
+    markdownlint-cli
+    nixd
+    nodePackages.bash-language-server
+    nodePackages.prettier
+    nodePackages.typescript
+    nodePackages.typescript-language-server
+    pyright
+    ruff
+    shellcheck
+    shfmt
+    stylua
+    terraform-ls
+    tflint
+    vscode-langservers-extracted
+    yaml-language-server
+  ];
+}
