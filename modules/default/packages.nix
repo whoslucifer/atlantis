a7e12187e7844cf997f4ee08f4e38bb1abd01fb9
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-12 17:07:00 +0300

    refactor: ditched home manager

--- modules/default/packages.nix
+++ modules/default/packages.nix
@@ -4,17 +4,105 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs;
+  with nodePackages_latest;
+  with libsForQt5; [
     gnumake
+
+    ghostty
+    anyrun
+    jq
+    tmux
+    wezterm
+
     xwayland
-    blender
     google-chrome
-    teams-for-linux
+    ani-cli
+    manga-cli
+    yt-dlp
+    telegram-desktop
+    vesktop
     localsend
 
+    starship
     #for ags new
-    cloudflare-warp
-    cloudflared
-    pinokio #installs ai tools locally
+    ##cloudflare-warp
+    #cloudflared
+    (mpv.override {scripts = [mpvScripts.mpris];})
+    d-spy
+    #figma-linux
+    kolourpaint
+    #github-desktop
+    nautilus
+    icon-library
+    dconf-editor
+    qt5.qtimageformats
+    yad
+    swaylock-effects
+
+    protonvpn-gui
+
+    #libreoffice-fresh
+    impression
+    spotdl
+    delta
+    #impression #rufus alternative
+    bat
+    eza
+    fd
+    ripgrep
+    fzf
+    tlrc
+    acpi
+    ffmpeg
+    libnotify
+    gobject-introspection
+    #killall
+    zip
+    unzip
+    glib
+    ydotool
+
+    #ddmp
+    zoxide
+    file
+    entr
+
+    # theming tools
+    gradience
+    gnome-tweaks
+
+    # hyprland
+    brightnessctl
+    cliphist
+    fuzzel
+    grim
+    hyprpicker
+    tesseract
+    imagemagick
+    pavucontrol
+    playerctl
+    swappy
+    slurp
+    swww
+    wayshot
+    wlsunset
+    wl-clipboard
+    wf-recorder
+
+    # langs
+    gjs
+    bun
+    cargo
+    #typescript
+    eslint
+    # very important stuff
+    uwuify
+
+    #ags new dots
+    ddcutil
+    bc
+    sass
+    unixtools.top
   ];
 }
