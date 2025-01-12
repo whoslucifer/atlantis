10e718749a729a318a42f53b317391749e6f86e2
鬼舞辻無惨 <samhansen.dev@gmail.com>
2025-01-12 17:08:16 +0300

    programs: hyprland xwayland enable=true

--- modules/default/programs.nix
+++ modules/default/programs.nix
@@ -1,33 +1,7 @@
-{
-  config,
-  pkgs,
-  host,
-  username,
-  options,
-  lib,
-  inputs,
-  system,
-  ...
-}: {
+{...}: {
   programs = {
     nm-applet.indicator = true;
-
-    virt-manager.enable = false;
-
-    #steam = {
-    #  enable = true;
-    #  gamescopeSession.enable = true;
-    #  remotePlay.openFirewall = true;
-    #  dedicatedServer.openFirewall = true;
-    #};
-
-    dconf.enable = true;
-    seahorse.enable = true;
-    fuse.userAllowOther = true;
-    mtr.enable = true;
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
+    hyprland.enable = true;
+    xwayland.enable = true;
   };
 }
