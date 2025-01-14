476bb41a3c7d7db5139d686a318956c1e7039067
Hansen <samhansen.dev@gmail.com>
2025-01-14 17:53:21 +0300

    feat(nvim): switched back to mason from nixpkgs

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -2,10 +2,11 @@ return {
   {
     "williamboman/mason.nvim",
     -- Disable Mason in favor of Nix packages
-    enabled = false,
+    enabled = true,
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "bash-language-server",
+        "typescript-language-server",
         "black",
         "goimports",
         "golangci-lint",
