faf4910bb17235a51fc523e14a002aee129f0021
Hansen <samhansen.dev@gmail.com>
2025-01-14 17:44:01 +0300

    fix(ags): packaged google-fonts gabarito

--- modules/default/theme.nix
+++ modules/default/theme.nix
@@ -13,8 +13,20 @@
       sed -i '/^Inherits=/ s/$/,breeze,adwaita/' $out/share/icons/index.theme || echo 'Inherits=breeze,adwaita' >> $out/share/icons/index.theme
     '';
   };
+
+  gabaritoFont = pkgs.stdenv.mkDerivation {
+    name = "gabarito-font";
+    src = pkgs.fetchurl {
+      url = "https://github.com/google/fonts/raw/main/ofl/gabarito/Gabarito%5Bwght%5D.ttf";
+      sha256 = "sha256-hlDivXdH99dGGf167LywMJ5vN7eWQCTz+xWuSDO2fKU=";
+    };
+    phases = ["installPhase"];
+    installPhase = ''
+      mkdir -p $out/share/fonts/truetype/google
+      cp $src $out/share/fonts/truetype/google/Gabarito-Regular.ttf
+    '';
+  };
 in {
-  # Add packages to the system
   environment.systemPackages = with pkgs; [
     adwaita-qt6
     adw-gtk3
@@ -23,15 +35,13 @@ in {
     bibata-cursors
   ];
 
-  # Install fonts using fonts.packages
   fonts = {
     packages = with pkgs; [
       nerd-fonts.space-mono
+      gabaritoFont
       nerd-fonts.ubuntu
       nerd-fonts.jetbrains-mono
       rubik
-      #google-fonts.Gabarito
-      #google-fonts.Lexend
       noto-fonts
       noto-fonts-emoji
       noto-fonts-cjk-sans
