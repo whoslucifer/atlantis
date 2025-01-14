68b90b4e5f00b5dbe28ae3fec930f514fa4d171f
Hansen <samhansen.dev@gmail.com>
2025-01-14 19:29:16 +0300

    refactor: major changes

--- modules/desktop/extra.nix
+++ modules/desktop/extra.nix
@@ -3,22 +3,6 @@
   pkgs,
   ...
 }: {
-  vm.guest-services.enable = false;
-  local.hardware-clock.enable = false;
-
-  # Extra Portal Configuration
-  xdg.portal = {
-    enable = true;
-    wlr.enable = false;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-    configPackages = [
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal
-    ];
-  };
-
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "schedutil";
--- modules/desktop/sessions.nix
+++ modules/desktop/sessions.nix
@@ -9,7 +9,17 @@
     nm-applet.indicator = true;
   };
 
-  xdg.portal.enable = true; #required for flatpak
+  xdg.portal = {
+    enable = true;
+    wlr.enable = false;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
     xwayland
