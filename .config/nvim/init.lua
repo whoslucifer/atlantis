6aac151ac921ba4852b794292086447c9eb3e042
Hansen <samhansen.dev@gmail.com>
2025-01-16 01:37:06 +0300

    feat(nvim): solarized dark osaka

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+if vim.loader then
+	vim.loader.enable()
+end
 
-vim.g.mapleader = " "
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
 
 require("config.lazy")
