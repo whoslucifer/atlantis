49d891806c0c637f65f5e3a31f112835881cf4fc
Hansen <samhansen.dev@gmail.com>
2025-01-19 01:35:40 +0300

    feat(fish): interactive aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,21 +19,54 @@ end
 
 zoxide init --cmd cd fish | source
 
-function delgen
-    sudo nix-env --profile /nix/var/nix/profiles/system/ --delete-generations
-end
-
-function lsgen
-    sudo nix-env --list-generations --profile /nix/var/nix/profiles/system/
-end
-
 alias cat=bat
+alias :q=exit
+alias lsgen='sudo nix-env --list-generations --profile /nix/var/nix/profiles/system/'
+alias delgen='sudo nix-env --profile /nix/var/nix/profiles/system/ --delete-generations'
+alias db=distrobox
+alias dbinit='distrobox create --name atlantis -i docker.io/kalilinux/kali-rolling:latest --init'
+alias kali='distrobox enter atlantis -- /usr/bin/fish'
+alias del='gio trash'
+alias dev='nix develop -c nvim'
+alias lg=lazygit
+alias ga='git add'
+alias gb='git branch'
+# alias gc='git commit'
+alias gch='git checkout'
+alias gcl='git clone'
+alias gd='git diff'
+alias gp='git push'
+alias gpm='git push -u origin main'
+alias gr='git reset --soft HEAD~1'
+alias gs='git status'
+alias hs='home-manager switch -b backup --flake .#asherah'
+alias l=ls
+alias la='ls -la'
+alias ns='sudo nixos-rebuild switch --flake .#nix'
+alias q=exit
+alias tree='eza --tree'
+alias v=nvim
+alias x=clear
+
+# Define completions for gdd
+complete -c gdd -f \
+    -a "(git diff --name-only --cached; git diff --name-only)"
+
+# Git-aware completions for gc (git commit), showing only modified files
+complete -c gc -f -a "(git diff --cached --name-only | grep -P -v '^\?')"
+
+function gdd
+    if test (count $argv) -eq 0
+        echo "Usage: gdd <file> or <revision> <file>"
+        return 1
+    end
+    bash -c "git diff -- $argv | delta"
+end
+
+function gc
+    git commit $argv
+end
 
 #set -Ux MSF_DATABASE_CONFIG /home/asherah/.msf4/database.yml
 
 set -Ux fish_user_paths /home/asherah/go/bin $fish_user_paths
-# function fish_prompt
-#   set_color cyan; echo (pwd)
-#   set_color green; echo '> '
-# end
-#uvx --generate-shell-completion fish | source
